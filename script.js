db5f077b720e9266da39b78559cccd6f274d115e
xvekii <vedran.dautanec@gmail.com>
2024-07-17 21:40:30 +0200

    Change slider value to 16, edit script to take input from slider, remove prompt, move border from script to .active

--- script.js
+++ script.js
@@ -3,29 +3,31 @@ const gridSlider = document.getElementById("grid-range");
 const gridSpanOutput = document.querySelector(".grid-slider-span");
 const containerWidth = 20.8;
 
+let cellsPerRow = 16;
+
+gridSlider.addEventListener("input", (event)=> {
+  gridSpanOutput.textContent = event.target.value;
+  cellsPerRow = parseInt(event.target.value);
+  createGrid();
+});
 
 function createGrid() {
   container.textContent = "";
-  gridSlider.addEventListener("input", (event)=> {
-    gridSpanOutput.textContent = event.target.value;
-    gridSpanOutput.textContent = gridSlider.value;
-  });
   
-  const cellsPerRow = +prompt("Enter dimensions (e.g. 4 for 4x4 grid): ", "16");
   let cellWidth = calculateCellWidth(cellsPerRow);
-  const totalCellNumber = (cellsPerRow * cellsPerRow);
+  const totalCellNumber = cellsPerRow * cellsPerRow;
 
   for (let i = 0; i < totalCellNumber; i++) {
     const div = document.createElement("div");
     div.classList.add("active");
-    div.style.cssText = `border: 0.1px solid black; height: ${cellWidth}rem; width: ${cellWidth}rem`;
+    div.style.cssText = `height: ${cellWidth}rem; width: ${cellWidth}rem`;
     
     container.appendChild(div);
   }
 }
 
 function calculateCellWidth(cellsPerRow) {
-  let finalCellWidth = (containerWidth / cellsPerRow);
+  let finalCellWidth = containerWidth / cellsPerRow;
   console.log(finalCellWidth);
   return finalCellWidth;
 }
