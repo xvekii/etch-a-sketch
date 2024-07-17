ca4ba5ed526e5ee06214619f9b42641b5d6274b2
xvekii <vedran.dautanec@gmail.com>
2024-07-17 21:06:13 +0200

    Reappropriate calculateContainerWidth to calculateCellWidth, remove setContainerWidth, set fixed width to .container

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container");
 const gridSlider = document.getElementById("grid-range");
 const gridSpanOutput = document.querySelector(".grid-slider-span");
+const containerWidth = 20.8;
 
 
 function createGrid() {
@@ -11,27 +12,22 @@ function createGrid() {
   });
   
   const cellsPerRow = +prompt("Enter dimensions (e.g. 4 for 4x4 grid): ", "16");
-  let containerWidth = calculateContainerWidth(cellsPerRow);
-  setContainerWidth(containerWidth);
+  let cellWidth = calculateCellWidth(cellsPerRow);
   const totalCellNumber = (cellsPerRow * cellsPerRow);
 
   for (let i = 0; i < totalCellNumber; i++) {
     const div = document.createElement("div");
     div.classList.add("active");
-    div.style.cssText = "border: 0.1px solid black; height: 1.3rem; width: 1.3rem";
+    div.style.cssText = `border: 0.1px solid black; height: ${cellWidth}rem; width: ${cellWidth}rem`;
     
     container.appendChild(div);
   }
 }
 
-function calculateContainerWidth(userInput) {
-  let containerWidth = (userInput * 1.3);
-  console.log(containerWidth);
-  return containerWidth;
-}
-
-function setContainerWidth(width) {
-  container.style.width = `${width}rem`;
+function calculateCellWidth(cellsPerRow) {
+  let finalCellWidth = (containerWidth / cellsPerRow);
+  console.log(finalCellWidth);
+  return finalCellWidth;
 }
 
 createGrid();
